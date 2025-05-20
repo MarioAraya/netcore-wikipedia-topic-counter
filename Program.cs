8a9dee2ced3e1cffc44b36f1a71ad218162146b7
Arayama <arayaromero@gmail.com>
2025-05-20 17:42:54 -0400

    Initial project setup with .gitignore, Program.cs, changelog, project file, and solution file

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace WikipediaTopicCounter;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Wikipedia Topic Counter - v0.1.0");
+        Console.WriteLine("================================");
+        Console.WriteLine("Este programa contará las ocurrencias de un tema en un artículo de Wikipedia.");
+    }
+}
---
+++ changelog.md
@@ -0,0 +1,9 @@
+# Changelog
+Todos los cambios notables en este proyecto serán documentados en este archivo.
+
+## [0.1.0] - 2025-05-20
+### Agregado
+- Creación inicial del proyecto de consola .NET Core
+- Estructura básica del proyecto
+- Archivo .gitignore
+- Inicialización del repositorio Git
